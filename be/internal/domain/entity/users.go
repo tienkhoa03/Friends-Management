c007ceea3e6903b7675f179cdaa1bf88ff5a4524
tienkhoa <tienkhoa03@gmail.com>
2025-07-30 16:31:25 +0700

    feat: add created time for users

--- be/internal/domain/entity/users.go
+++ be/internal/domain/entity/users.go
@@ -1,6 +1,9 @@
 package entity
 
+import "time"
+
 type User struct {
-	Id    int64  `gorm: "primaryKey;autoIncrement" json:"id"`
-	Email string `gorm:"type:varchar(256);not null;unique" json:"email"`
+	Id        int64  `gorm: "primaryKey;autoIncrement" json:"id"`
+	Email     string `gorm:"type:varchar(256);not null;unique" json:"email"`
+	CreatedAt time.Time
 }
